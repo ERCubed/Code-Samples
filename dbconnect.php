5810cac309df80d940859195d624ea1a8bcb6211
Eric Roberts <eric@ercubed.com>
2012-02-13 11:25:54 -0600

    Removed all actual login information, making this generic, and preserving the site's security.

--- dbconnect.php
+++ dbconnect.php
@@ -1,23 +1,21 @@
 <?
-// local values: "localhost", "root", ""
-// live values: "p50mysql211.secureserver.net","copperlampdb","aSimple1"
 
 if (strpos($_SERVER['HTTP_REFERER'],"copperlamp")) {
 	// Use Live Values
-	$dbserver = "p50mysql211.secureserver.net";
-	$dbuser = "copperlampdb";
-	$dbpass = "aSimple1";
+	$dbserver = "LIVE_SERVER_NAME";
+	$dbuser = "LIVE_DB_NAME";
+	$dbpass = "LIVE_PASSWORD";
 } else {
 	// Use Local Values
 	$dbserver = "localhost";
-	$dbuser = "copperlampdb";
-	$dbpass = "aSimple1";
+	$dbuser = "LOCAL_DB_NAME";
+	$dbpass = "LOCAL_PASSWORD";
 }
 
 
-if (!mysql_connect("localhost","copperlampdb","aSimple1")) {
+if (!mysql_connect($dbserver,$dbuser,$dbpass)) {
 	echo "Unable to connect: ".$TEXT['cds-error'];
 	die();
 }
-mysql_select_db("copperlampdb");
+mysql_select_db("NAME_OF_DB");
 ?>
